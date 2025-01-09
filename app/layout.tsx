19118d8d756af22e574787e5a45523f97c94564d
SanyamPunia <lewarends@gmail.com>
2025-01-09 12:48:38 +0530

    feat: :chart_with_upwards_trend: init analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,7 +25,7 @@ export const metadata: Metadata = {
     "code sharing",
     "developer tools",
     "programming",
-    "gist"
+    "gist",
   ],
   authors: [{ name: "sanyam punia" }],
   openGraph: {
@@ -80,6 +81,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-zinc-950`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
